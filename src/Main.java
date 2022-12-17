25c3af67cfcab5dfaf6f9d57e872f9e022260287
Irina.Test <ira.test2022@gmail.com>
2022-12-17 19:54:55 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInKopecks = 10_000;
+        int percentInKopecks = 200;
+        int bonus = amountInKopecks / percentInKopecks;
+        System.out.println("Ваши бонусы + мили составили:"+ bonus +" miles");
+    }
+}
